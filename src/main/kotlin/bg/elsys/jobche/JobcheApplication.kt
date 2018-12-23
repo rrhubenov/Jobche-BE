120aa28386e263cd1ab22a7d28052b1f20fd5e01
Radoslav Hubenov <rhubenov@appolica.com>
2018-12-23 21:09:57 +0200

    #17 config App for Tomcat
    Added needed config for deploying to Tomcat. Now everything should be working fine

--- src/main/kotlin/bg/elsys/jobche/JobcheApplication.kt
+++ src/main/kotlin/bg/elsys/jobche/JobcheApplication.kt
@@ -2,10 +2,17 @@ package bg.elsys.jobche
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
 
 @SpringBootApplication
-class JobcheApplication
-
+class JobcheApplication : SpringBootServletInitializer(){
+    override fun configure(builder: SpringApplicationBuilder?): SpringApplicationBuilder {
+        return builder!!.sources(JobcheApplication::class.java)
+    }
+}
 fun main(args: Array<String>) {
     runApplication<JobcheApplication>(*args)
 }
+

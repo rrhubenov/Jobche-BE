60feca3ac3640941d72005dc326489616831fb0f
Radoslav Hubenov <rhubenov@appolica.com>
2019-01-12 21:26:55 +0200

    #23:User-Repository-Methods
    Added a method for finding a user with email and deleting with email

--- src/main/kotlin/bg/elsys/jobche/repositories/UserRepository.kt
+++ src/main/kotlin/bg/elsys/jobche/repositories/UserRepository.kt
@@ -3,9 +3,14 @@ package bg.elsys.jobche.repositories
 import bg.elsys.jobche.entity.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun findByEmail(username: String) : User?
     fun existsByEmail(email: String): Boolean
+    @Transactional
+    fun deleteByEmail(email: String)
+    @Transactional
+    fun getOneByEmail(email: String): User
 }
